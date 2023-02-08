f4e93c67002fd0bd59f815f11cf5e66fb41fc9d2
ConcernedMotley <123102312+ConcernedMotley@users.noreply.github.com>
2023-02-08 15:39:05 -0700

    Update Sprint1_Demo.py

--- Sprint1_Demo.py
+++ Sprint1_Demo.py
@@ -1,3 +1,14 @@
 from Course import *
 from Cohort import *
 
+courseName = "Courses_eg.csv"
+
+courses_01 = readSaveCourse(courseName)
+
+
+if __name__ == "__main__":
+
+    print("\nCohort and Course Sprint 1 Demo\n")
+
+    displaySavedCourses(courses_01)
+
