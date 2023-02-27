273fcc7be32ce28c13418c7e1552cc16acbe1192
Schuyler Kelly <kellys64@mymacewan.ca>
2023-02-27 12:28:59 -0700

    Committing changes before pull

--- Students/students.py
+++ Students/students.py
@@ -1,9 +1,10 @@
 '''
-Author: Fahad Ali
-Student ID: 3099218
+Author: Fahad Ali, Schuyler Kelly
 Description: This program will create cohorts of the best size depending on
 the class space available, and suggest (if required) more class space
 '''
+
+import math
 
 class Classroom:
 
@@ -67,6 +68,49 @@ class students:
 
         return(cohortList)
 
+    def _rebalance_cohort_list(self, cohortProgramList):
+
+        new_cohort_list = []
+
+        # Iterate through each program and see if it fills a classroom
+        for cohort in cohortProgramList:
+            cohort = cohort.split(",")
+            occupied, space = [int(x) for x in cohort[1].strip().split("/")]
+
+            # Get ten percent of the space available
+            ten_percent_of_space = int(0.15 * space)
+
+            # If the cohort isn't within ten percent of the total space, we need to rebalance
+            if not math.isclose(occupied, space, abs_tol=ten_percent_of_space):
+                for room in self._classrooms:
+                    # Set continue clause to avoid unecessary nesting
+                    if room._in_use:
+                        continue
+                    # Check if room space is within acceptable range
+                    if math.isclose(occupied, room._cap, abs_tol=int(0.15 * room._cap)):
+                        new_cohort_list.append(f"{cohort[0]}: {room._name}, {occupied}/{room._cap}")
+
+                        # Now reset old room as available
+                        for room in self._classrooms:
+
+                            # cohort[0].split(" ")[1] is the room number
+                            if room._name == cohort[0].split(" ")[1]:
+                                room._current_students = room._cap
+                                room._in_use = False
+                                break
+                        break
+            else:
+                new_cohort_list.append(f"{cohort[0]}{cohort[1]}")
+
+        print("OLD LIST ================")
+        for cohort in cohortProgramList:
+            print(cohort)
+
+        print("NEW LIST ================")
+        for cohort in new_cohort_list:
+            print(cohort)
+        print()
+
     def divide_to_cohorts(self, students, ID):
         cohortDict = {0:"BC", 1: "PC", 2:"PM", 3:"BA", 4:"GL", 5:"FS", 6:"DXD",\
                         7:"BK", 8:"SCM"}
@@ -113,6 +157,11 @@ class students:
 
                 # Set the classroom boolean as in use
                 classroom._in_use = True
+
+        # ===== REBALANCE COHORT LIST IF OFF BALANCE =====
+        if cohortProgramList:
+            self._rebalance_cohort_list(cohortProgramList)
+
 
         # If the ID is one 1, reset dictionary as we are moving on to electives
         if ID == 1:
