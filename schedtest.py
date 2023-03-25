978a77d6832185075c0604889bf08779f6f82a59
Mike Lee <leem252@mymacewan.ca>
2023-03-25 16:19:14 -0600

    shedtest

--- schedtest.py
+++ schedtest.py
@@ -15,24 +15,26 @@ from lib.fileio import getClassrooms
 week = Week(0)
 def scheduleCourses(week, cohorts):
     for cohort in cohorts:
-        courseQueue = deque(cohort.program.term1)
+        courseQueue = deque(cohort.programCourses.term1)
 
-        if "BCOM" in cohort.name or "PCOM" in cohort.name:
+        if "BCOM" in cohort.cohortName or "PCOM" in cohort.cohortName:
             days = [week.getDay("Monday"), week.getDay("Wednesday")]
         else:
             days = [week.getDay("Tuesday"), week.getDay("Thursday")]
 
         while courseQueue:
             currentCourse = courseQueue.popleft()
-            prefClassroomName = cohort.clasroom.classRoomNumer
+            prefClassroomName = cohort.classroom.classRoomNumber
             day1 = days[0]
             day2 = days[1]
+            someList = someList.append(day1,day2)
             for i in range(0, len(week.days.classrooms)):
-                if (prefClassroomName == day1.classrooms[i] and day1.classrooms[i].inUse is False) and \
-                   (prefClassroomName == day2.classrooms[i] and day2.classrooms[i].inUse is False):
+                if (prefClassroomName == day1.classrooms[i].classRoomNumber and day1.classrooms[i].inUse is False) and \
+                   (prefClassroomName == day2.classrooms[i].classRoomNumber and day2.classrooms[i].inUse is False):
+
                     startTime, endTime = scheduleLecture(currentCourse.lecturelength,
                                                          week.days.classrooms[i].currentBlockTime)
-                    newBlock = timeBlock(startTime, endTime, cohort.cohortname, currentCourse.courseName, 0, 0)
+                    newBlock = timeBlock(startTime, endTime, cohort.cohortName, currentCourse.courseName, 0, 0)
                     day1.classrooms[i].timeBlocks.append(newBlock)
                     day2.classrooms[i].timeBlocks.append(newBlock)
     return copy.deepcopy(week)
