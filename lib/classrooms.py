6444e0bf63aafecc8d99d20c5b0532483b7b3925
Mike Lee <leem252@mymacewan.ca>
2023-03-25 13:11:22 -0600

    added currentBlockTime in classrooms.py

--- lib/classrooms.py
+++ lib/classrooms.py
@@ -29,21 +29,22 @@ class Classroom:
         self.classRoomNumber = classRoomNumber
         self.normalCapacity = normalCapacity
         self.currentStudents = normalCapacity
+        self.currentBlockTime = None
         self.lab = lab
         self.inUse = False
         self.isGhost = False
-<<<<<<< HEAD
         self.noSpaceForBlocks = False
         self.timeBlocks = None
-=======
         
     def __repr__(self):
         return f"Classroom(name={self.classRoomNumber}, cap={self.normalCapacity})"
->>>>>>> 3994a0f42975f9c965bbdb93e01012bea1dd38b2
 
     def setGhost(self):
         self.isGhost = True
 
+    def setBlockTime(self, time):
+        self.currentBlockTime = time
+        
     def addBlock(self, newTimeBlock):
         self.timeBlocks.append(newTimeBlock)
 
